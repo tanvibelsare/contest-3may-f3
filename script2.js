63d60103c1d0249bedf747a09f418d5fc359f6dc
Tanvi Belsare <tanvibelsare11@gmail.com>
2023-05-27 23:32:20 +0530

    first commit

---
+++ script2.js
@@ -0,0 +1,15 @@
+let Container=document.getElementById("container")
+//converting stringify version of object
+let myUser=JSON.parse(localStorage.getItem("User"))
+// console.log(myUser)
+Container.innerHTML=`<p>Profile</p>
+<p>Full Name:${myUser.username}</p>
+<p>Email:${myUser.email}</p>
+<p>Password:${myUser.password}</p>
+<button id="btn">Logout</button>`
+let button=document.getElementById("btn")
+// console.log(button)
+button.addEventListener("click",()=>{
+    console.log("click")
+    localStorage.removeItem("User");
+})
